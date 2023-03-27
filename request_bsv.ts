1e7ecb89711a3b0aaf5ba473b44518500e6be4dc
Zyler <508282+zylerdj@users.noreply.github.com>
2023-03-27 20:13:31 +0000

    Example for Requesting Multi-Output BSV

---
+++ request_bsv.ts
@@ -0,0 +1,37 @@
+
+require('dotenv').config()
+
+import anypay from 'anypay'
+
+export async function main() {
+
+  let paymentRequest = await anypay.request([{
+
+    currency: 'BSV',
+
+    to: [{
+      address: '1C9jt1rdpjvhEgT7TirEnxrz2eVpS3vqfZ',
+      amount: 52.00,
+      currency: 'USD'
+    }, {
+      address: '123AeiwXiYswoNrNRCvPekkKJYvoUzyxbC',
+      amount: 8.00,
+      currency: 'USD'
+    }, {
+      address: '123AeiwXiYswoNrNRCvPekkKJYvoUzyxbC',
+      amount: 0.001,
+      currency: 'USD'
+    }]
+
+  }], {
+
+    webhook: 'https://example.com/anypay-webhooks'
+
+  })
+
+  console.log(paymentRequest)
+
+}
+
+main()
+
